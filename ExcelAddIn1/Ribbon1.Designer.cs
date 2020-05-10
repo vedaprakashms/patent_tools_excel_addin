9b17ccd680704857a0b11ef5e7579c9f1b62f1c6
Veda Prakash <vedaprakash.ms@outlook.com>
2020-05-10 17:01:25 +0530

    update for addin for google patents and espacenet

---
+++ ExcelAddIn1/Ribbon1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ExcelAddIn1
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Patent_tools = this.Factory.CreateRibbonTab();
+            this.Hyperlinker = this.Factory.CreateRibbonGroup();
+            this.G_patents = this.Factory.CreateRibbonButton();
+            this.Espacenet = this.Factory.CreateRibbonButton();
+            this.USPTO = this.Factory.CreateRibbonButton();
+            this.PDF_Link = this.Factory.CreateRibbonGroup();
+            this.Patent_tools.SuspendLayout();
+            this.Hyperlinker.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Patent_tools
+            // 
+            this.Patent_tools.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.Patent_tools.Groups.Add(this.Hyperlinker);
+            this.Patent_tools.Groups.Add(this.PDF_Link);
+            this.Patent_tools.Label = "Patent_Tools";
+            this.Patent_tools.Name = "Patent_tools";
+            // 
+            // Hyperlinker
+            // 
+            this.Hyperlinker.Items.Add(this.G_patents);
+            this.Hyperlinker.Items.Add(this.Espacenet);
+            this.Hyperlinker.Items.Add(this.USPTO);
+            this.Hyperlinker.Label = "Hyperlinker";
+            this.Hyperlinker.Name = "Hyperlinker";
+            // 
+            // G_patents
+            // 
+            this.G_patents.Label = "G_patents";
+            this.G_patents.Name = "G_patents";
+            this.G_patents.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.G_patents_Click);
+            // 
+            // Espacenet
+            // 
+            this.Espacenet.Label = "Espacenet";
+            this.Espacenet.Name = "Espacenet";
+            this.Espacenet.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Espacenet_Click);
+            // 
+            // USPTO
+            // 
+            this.USPTO.Label = "USPTO";
+            this.USPTO.Name = "USPTO";
+            this.USPTO.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.USPTO_Click);
+            // 
+            // PDF_Link
+            // 
+            this.PDF_Link.Label = "PDF links";
+            this.PDF_Link.Name = "PDF_Link";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.Patent_tools);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.Patent_tools.ResumeLayout(false);
+            this.Patent_tools.PerformLayout();
+            this.Hyperlinker.ResumeLayout(false);
+            this.Hyperlinker.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab Patent_tools;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Hyperlinker;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton G_patents;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton Espacenet;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton USPTO;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup PDF_Link;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ExcelAddIn1/Ribbon1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using office = Microsoft.Office.Core;
+using System.Text.RegularExpressions;
+
+namespace ExcelAddIn1
+{
+    public partial class Ribbon1
+    {
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void G_patents_Click(object sender, RibbonControlEventArgs e)
+        {
+            
+            Excel.Worksheet activeworksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range range = Globals.ThisAddIn.Application.Selection;
+            foreach(Excel.Range k in range.Cells)
+            {
+                string pattern = "\\s+";
+                string temp = k.Text;
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string result = rgx.Replace(temp, replacement);
+                k.Value2 = result;
+                k.Hyperlinks.Add(k, "https://patents.google.com/patent/" + result+"/en");
+            }
+        }
+
+        private void Espacenet_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeworksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range range = Globals.ThisAddIn.Application.Selection;
+            foreach (Excel.Range k in range.Cells)
+            {
+                string pattern = "\\s+";
+                string temp = k.Text;
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string result = rgx.Replace(temp, replacement);
+                k.Value2 = result;
+                k.Hyperlinks.Add(k, "https://worldwide.espacenet.com/patent/search?q=" + result );
+            }
+        }
+
+        private void USPTO_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeworksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range range = Globals.ThisAddIn.Application.Selection;
+            foreach (Excel.Range k in range.Cells)
+            {
+                string pattern = "\\s+";
+                string temp = k.Text;
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string result = rgx.Replace(temp, replacement);
+                k.Value2 = result;
+                if (result.Length > 8)
+                {
+                    //http://patft.uspto.gov/netacgi/nph-Parser?TERM1=10298608&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50;
+                    k.Hyperlinks.Add(k, "https://worldwide.espacenet.com/patent/search?q=" + k.Text);
+                }
+                else
+                {
+                    k.Hyperlinks.Add(k, "https://worldwide.espacenet.com/patent/search?q=" + k.Text);
+                }
+                
+            }
+        }
+
+        private void PDF_USPTO_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet activeworksheet = Globals.ThisAddIn.Application.ActiveSheet;
+            Excel.Range range = Globals.ThisAddIn.Application.Selection;
+            foreach (Excel.Range k in range.Cells)
+            {
+                k.Hyperlinks.Add(k, "https://worldwide.espacenet.com/patent/search?q=" + k.Text);
+                
+            }
+        }
+    }
+}
